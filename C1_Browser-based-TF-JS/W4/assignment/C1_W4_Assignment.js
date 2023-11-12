ba8e125fa178ef2020e78745c47fdab3c9f9a3e4
Wildan <143121314+wildandz@users.noreply.github.com>
2023-11-13 01:16:28 +0700

    Update C1_W4_Assignment.js
    Need

--- C1_Browser-based-TF-JS/W4/assignment/C1_W4_Assignment.js
+++ C1_Browser-based-TF-JS/W4/assignment/C1_W4_Assignment.js
@@ -27,18 +27,23 @@ async function train() {
     layers: [
         
       // YOUR CODE HERE
-
+	    
+      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+      tf.layers.dense({ units: 100, activation: 'relu'}),
+      tf.layers.dense({ units: 5, activation: 'softmax'})
     ]
   });
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
   const optimizer = // YOUR CODE HERE
+	  tf.train.adam(0.0001);
     
         
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
   model.compile(// YOUR CODE HERE);
+	  {optimizer: optimizer, loss: 'categoricalCrossentropy'});
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -76,9 +81,13 @@ function handleButton(elem){
         // HINT: Look at the previous cases.
             
         // YOUR CODE HERE
-		
+	 case "4":
+            lizardSamples++;
+            document.getElementById("lizardsamples").innerText = "Lizard samples:" + lizardSamples;
+            break;
             
-	}
+	}	
+            
 	label = parseInt(elem.id);
 	const img = webcam.capture();
 	dataset.addExample(mobilenet.predict(img), label);
@@ -113,7 +122,9 @@ async function predict() {
         // HINT: Look at the previous cases.
             
         // YOUR CODE HERE 
-	
+	case 3:
+			predictionText = "I see Lizard";
+			break;
             
 	}
 	document.getElementById("prediction").innerText = predictionText;
